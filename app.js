428d4d90a3b6d984fe1b40645a87bda3b601eddf
Stuart Wade <stuartwade911@gmail.com>
2021-05-05 18:19:50 +0200

    Create app.js

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 //declaing all of the different holiday dates that i will be using 
-const newYears = new Date('Jan 1, 2022 00:00:00').getTime();
+const newYears = new Date('Jan 1, 2021 00:00:00').getTime();
 const christmas = new Date('Dec 25, 2021 00:00:00').getTime();
-const valentines = new Date('Feb 14, 2022 00:00:00').getTime();
+const valentines = new Date('Feb 14, 2021 00:00:00').getTime();
 const eastor = new Date('Apr 4, 2021 00:00:00').getTime();
 const thanksgiving = new Date('Nov 25, 2021 00:00:00').getTime();
 
+//value of one year in seconds. This is used to increment the year when the current date is past the selected holiday
+var yearValue = 31536000000;
+
+//the following two lines gets the current year and saves it into a variable we will be used later
+var d = new Date();
+var currentYear = d.getFullYear();
 
 
 // variable holding my timer. Will be used to set intervals and clear intervals
-var myTimer;
-
 
 // setting a default interval for the time to start when webpage loads
 // the default is set to show countdown to New Years 2022
-myTimer = setInterval(function(){calculateTimeLeft(newYears)}, 1000);
+myTimer = setInterval(function(){calculateTimeLeft(newYears + yearValue)}, 1000);
 
 
     //calling the sellection element to check which holiday the user selected
@@ -29,48 +33,65 @@ myTimer = setInterval(function(){calculateTimeLeft(newYears)}, 1000);
 
         //if statement varifies which option was chosen and calls function to change the counter
         if (option == 1) {
-            changeHoliday(christmas,'christmas.jpg','Countdown to Chistmas', 'Dec 25th 2021');  
+            changeHoliday(christmas,'christmas.jpg','Countdown to Chistmas', 'Dec 25th ');  
         }else if(option == 2){
-            changeHoliday(valentines,'valentines.jpg','Countdown to Valentines Day', 'Feb 14th 2021');  
+            changeHoliday(valentines,'valentines.jpg','Countdown to Valentines Day', 'Feb 14th ');  
         }
         else if(option == 3){
-            changeHoliday(eastor,'eastor.jpg','Countdown to Eastor', 'Apr 4th 2021');  
+            changeHoliday(eastor,'eastor.jpg','Countdown to Eastor', 'Apr 4th ');  
         }
         else if(option == 4){
-            changeHoliday(thanksgiving,'thanksgiving.jpg','Countdown to Thanksgiving', 'Nov 25th 2021');  
+            changeHoliday(thanksgiving,'thanksgiving.jpg','Countdown to Thanksgiving', 'Nov 25th ');  
         }else {
-            changeHoliday(newYears,'new_years.jpg','Countdown to New Years','Jan 1st 2020');  
+            changeHoliday(newYears,'new_years.jpg','Countdown to New Years','Jan 1st ');  
         }
     })
 
+    //this fuction changes the information displayed when the user selects a different holiday
     function changeHoliday(holiday,background,string,date){
 
         document.body.style.background = background;
 
+        //this line simply gets the current date value. 
+        var now = new Date().getTime();
+
+        //this if statement checks if this current date is past the holiday. If that is the case it increments the year 
+        //so that we can accurately calculate the remaining time till the selected holiday.
+        //we increase the year by adding the yearValue variable wo saved earlier.
+        if (holiday < now) 
+            {
+                date += (currentYear+1);
+                holiday += yearValue;
+            }
+            else{   //else if the selected holiday is still coming up we 
+                date +=currentYear;
+            }
+
+        //here we are changing the displayed holiday information to the currently selected holidays information. 
         document.getElementById('countdownString').innerText = string
         document.getElementById('date').innerText = date
-        if (holiday == valentines ) {
-            document.getElementById('date').innerText = "Feb 14th 2022"
-        }
-        else if(holiday == newYears){
-            document.getElementById('date').innerText = "Jan 1st 2022"
-        }
 
+        //here we are clearing/resetting the timer 
         if (myTimer != null) {
             clearTimer();
         }
+        //updating the timer
         myTimer = setInterval(function(){calculateTimeLeft(holiday)}, 1000);
         
         
     }
 
+//here we are clearing/resetting the timer intervals
 function clearTimer() {
     clearInterval(myTimer);
 }
     
-
+//this fuction we are simply getting the difference between the current date and time and the selected holiday
+//then we convert the difference into readable data to show in the timer.
 function calculateTimeLeft(then){
+    //this line is getting the current time and date
     var now = new Date().getTime();
+    //this line is getting the difference(gap) between now and the selected holiday
     var gap = then - now;
 
     var seconds = 1000;
